4ab05e13794a14b8e6d9ecae3e29566c1dca40fc
kaio <kaioa9851@gmail.com>
2023-08-25 22:57:25 -0300

    Ptac
    Atividade

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
+    const [Atividade, setAtividade] = useState("");
+    
+
    
     return (
         <div>
