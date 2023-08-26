ef33abbde89a9ed0ccf466ca29194ad04405a13e
kaio <kaioa9851@gmail.com>
2023-08-25 23:08:03 -0300

    PTAC2
    Atividade

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -3,13 +3,36 @@ import { Link } from "react-router-dom";
 
 export default function ToDo() {
     const [Atividade, setAtividade] = useState("");
-    
+    const [Lista, setLista] = useState([]);
 
+    const [id, setId] = useState(1)
+
+    const salvar = (e) =>{
+        e.proventDefault();
+        setLista([...Lista,{
+            atividade: atividade, id:id
+        }])
+
+        setId(1 + 1)
+        console.log(Lista)
+    }
    
     return (
         <div>
-            <h1>Lista de Atividades</h1>
             <Link to="/">home</Link>
+            <h1>Lista de Atividades</h1>
+
+            <form onSubmit={salvar}>
+                <input type="text"
+                  onRateChange={(e) =>{setAtividade(e.target.value)}}/>
+                  <button>ADD</button>
+            </form>
+
+            {Lista.map((ativ)=>
+               <div>
+                <p>{ativ.atividade}</p>
+               </div>
+            )}
            
         </div>
     );
