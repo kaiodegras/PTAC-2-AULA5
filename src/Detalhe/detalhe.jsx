2f244c6e3f21084b6a42792bef3f4c7c6fba3b54
kaio <kaioa9851@gmail.com>
2023-10-27 19:02:12 -0300

    Atividade PTAC

---
+++ src/Detalhe/detalhe.jsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+export default function Detalhe(){
+
+    const {id} = useParams();
+     return(
+        <div>
+            <h1>Olá {id} </h1>
+        </div>
+     )
+}
+
+
+
